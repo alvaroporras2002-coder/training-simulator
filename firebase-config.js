96f229cc852394a29e76fd10ab6e462c0d645ad3
alvaroporras2002-coder <alvaro.porras2002@gmail.com>
2026-08-21 16:39:20 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 window.TRAINING_FIREBASE_CONFIG = Object.freeze({
-  apiKey: "AIzaSyC4hGg_9MFBM0Mtb0_A01Tv9nWyKAJNYe0",
+  apiKey: "AIzaSyC4hGg_9MFMBOMtb0_A01Tv9nWyKAJNye0",
   authDomain: "training-simulator-214c4.firebaseapp.com",
   projectId: "training-simulator-214c4",
   storageBucket: "training-simulator-214c4.firebasestorage.app",
   messagingSenderId: "619140038475",
   appId: "1:619140038475:web:27d064058cde85f62cf2ce"
 });
-
 window.TRAINING_APP_SETTINGS = Object.freeze({
   appName: "Merchant Portal Training",
   appVersion: "13.10-registration-1",
